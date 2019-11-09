c7217266140017acbe3c9e42da819f70bacb93d4
Luke Spademan <info@lukespademan.com>
2019-11-09 14:59:05 +0000

    Added some badly written stuff as per

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import random
+
 WIDTH = 500
 HEIGHT = 500
 
@@ -8,12 +10,40 @@ BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
 pet_image = "snake"
+pet_dead_image = "tombstone"
 
 pet = Actor(pet_image)
 pet.pos = WIDTH / 2, HEIGHT / 2
+energy = 5
+
+feed_btn = Rect((20, 20), (100, 100))
+
+
+def on_mouse_down(pos, button):
+    if button == mouse.LEFT and feed_btn.collidepoint(pos):
+        print("FOOD")
+
+
+def get_hungry():
+    global energy
+    energy -= 1
+    clock.schedule(get_hungry, random.randint(2, 5))
+
+
+def die():
+    pet.image = pet_dead_image
+
+
+def update():
+    if energy <= 0:
+        die()
 
 
 def draw():
     screen.clear()
     screen.fill(BLACK)
     pet.draw()
+    screen.draw.textbox(f"Feed Me {energy}", feed_btn, background=GREEN)
+
+
+get_hungry()
