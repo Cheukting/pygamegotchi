59a713abf55eb445e15616dabfb81ef85df729ee
Luke Spademan <info@lukespademan.com>
2019-11-09 15:17:16 +0000

    Created pet class etc...

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import random
 
+
 WIDTH = 500
 HEIGHT = 500
 
@@ -9,41 +10,82 @@ GREEN = (0, 255, 0)
 BLUE = (0, 0, 255)
 WHITE = (255, 255, 255)
 
-pet_image = "snake"
-pet_dead_image = "tombstone"
 
-pet = Actor(pet_image)
-pet.pos = WIDTH / 2, HEIGHT / 2
-energy = 5
+class Pet:
+    def __init__(self, img, dead_img):
+        self.img = img
+        self.dead_img = dead_img
+
+        self.alive = True
+        self.energy = 5
+
+        self.actor = Actor(self.img)
+        self.move(WIDTH / 2, HEIGHT / 2)
+
+    def die(self):
+        self.actor.image = self.dead_img
+        self.alive = False
+
+    def draw(self):
+        self.actor.draw()
+
+    def move(self, x, y):
+        self.actor.pos = x, y
+
+    def get_hungry(self):
+        self.energy -= 1
+        clock.schedule(self.get_hungry, random.randint(2, 5))
+
+    def update(self):
+        if self.energy <= 0:
+            self.die()
+
+    def start(self):
+        self.get_hungry()
+
+    def feed(self):
+        self.energy += 1
+        if self.energy > 10:
+            self.energy = 10
+
+
+pet = Pet("snake", "pet_dead")
+pet.start()
+
 
 feed_btn = Rect((20, 20), (100, 100))
+reset_btn = Rect((20, 20), (100, 100))
+
+
+def reset():
+    global pet
+    pet = Pet("snake", "pet_dead")
+    pet.start()
 
 
 def on_mouse_down(pos, button):
-    if button == mouse.LEFT and feed_btn.collidepoint(pos):
-        print("FOOD")
-
-
-def get_hungry():
-    global energy
-    energy -= 1
-    clock.schedule(get_hungry, random.randint(2, 5))
+    if button == mouse.LEFT:
+        if pet.alive:
+            if feed_btn.collidepoint(pos):
+                pet.feed()
+        else:
+            if reset_btn.collidepoint(pos):
+                reset()
 
 
 def die():
-    pet.image = pet_dead_image
+    pet.die()
 
 
 def update():
-    if energy <= 0:
-        die()
+    pet.update()
 
 
 def draw():
     screen.clear()
     screen.fill(BLACK)
     pet.draw()
-    screen.draw.textbox(f"Feed Me {energy}", feed_btn, background=GREEN)
-
-
-get_hungry()
+    if pet.alive:
+        screen.draw.textbox(f"Feed Me {pet.energy}", feed_btn, background=GREEN)
+    else:
+        screen.draw.textbox(f"Reset", reset_btn, background=RED)
